324d7c4036733d86209c28952ed46e50c8c3fa9d
0xdreamm <154840885+0xdreamm@users.noreply.github.com>
2024-12-29 13:46:41 +0300

    Add files via upload

---
+++ enc.py
@@ -0,0 +1,25 @@
+ghio='r'
+uuyui='e'
+jffjfjdj=ghio+uuyui
+
+"""
+
+yoyoyo
+wussup
+"""
+hu='gi'
+ht='t'
+ui='hu'
+yoyoyowssub='a'
+fucker='o'
+ttrt='t'
+xx='0xdrea'
+xxx=xx+'m'
+dhdheue7i9q9o2fhdudu=jffjfjdj
+print("TELE : @DDr0x")
+io='p'
+iu='y'
+iop=io+iu
+drdr=f"reques{ttrt}s"
+asd='ge'
+asdd=asd+ttrt
